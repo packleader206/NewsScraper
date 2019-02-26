31e9f50fbffd7481187259adb4e657c9b4db91fc
packleader206 <packleader206@gmail.com>
2019-02-26 11:31:31 -0800

    minor format changes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsscraper";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-  //useMongoClient: true
+  useMongoClient: true
 });
 
 // Import routes and give the server access to them.
